894c46019625ca1fdb335fee8a8a91679a05e4ab
Ozan Yetkin <ozan.yetkin.579@gmail.com>
2024-05-27 11:48:05 +0300

    initialized with hello world, pointers and memory to be added

---
+++ comments.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main()
+{
+    // This is a single line comment
+    printf("Chocolate chip cookies are way better than oatmeal raisin cookies.\n");
+
+    /*
+    This is
+    a multiline
+    comment
+    */
+    printf("Learning the C programming language is an exciting adventure!\n");
+}
---
+++ hello.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/*
+int main() {
+    // output a line
+    printf("Hello World!\n");
+}
+*/
+
+int main()
+{
+    // Simple Recipe
+    printf("2 Cups: All Purpose Flour\n");
+    printf("2/3 Cups: Granulated Sugar\n");
+    printf("1 Cups: Unsalted Butter\t(Room Temperature)");
+}
