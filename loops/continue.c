9396af3d4eda5e3d2fb0ab35a1d6445db6be6602
Ozan Yetkin <ozan.yetkin.579@gmail.com>
2024-05-27 15:59:10 +0300

    loops finished, arrays initialized

---
+++ loops/continue.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()
+{
+
+    for (int i = 0; i < 10; i++)
+    {
+        // Figure out how to skip the print of 5 here
+        if (i == 5)
+        {
+            continue;
+        }
+
+        printf("%d\n", i);
+    }
+}
---
+++ loops/while.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main()
+{
+
+    int i = 9;
+    int square = 0;
+
+    // Write a while loop here:
+    while (i >= 0)
+    {
+        printf("%d\t%d\n", i, i * i);
+        i--;
+    }
+}
